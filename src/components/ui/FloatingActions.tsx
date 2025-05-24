e36d2025c7397ae880284563f5b2983211f92bf0
sherazrazadev <sherazrazadev@gmail.com>
2025-05-24 10:56:14 +0500

    update floating action import issue

--- src/components/ui/FloatingActions.tsx
+++ src/components/ui/FloatingActions.tsx
@@ -1,6 +1,5 @@
 // src/components/ui/FloatingActions.tsx - Fixed positioning and responsive
 import React from 'react';
-// import WhatsAppButton from './WhatsAppButton';
 import ChatBot from './ChatBot';
 import WhatsAppButton from './whatsappButton';
 
